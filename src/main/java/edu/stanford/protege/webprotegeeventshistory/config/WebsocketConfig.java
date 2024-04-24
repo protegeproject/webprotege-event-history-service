e4f3cb194f92c06db149092c69214cad6d188dab
silag <silaghialexmihail@gmail.com>
2024-04-24 09:20:41 +0300

    added validation on subscribe

--- src/main/java/edu/stanford/protege/webprotegeeventshistory/config/WebsocketConfig.java
+++ src/main/java/edu/stanford/protege/webprotegeeventshistory/config/WebsocketConfig.java
@@ -1,9 +1,12 @@
 package edu.stanford.protege.webprotegeeventshistory.config;
 
+import edu.stanford.protege.webprotegeeventshistory.AccessManager;
 import jakarta.websocket.ContainerProvider;
 import jakarta.websocket.WebSocketContainer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.client.standard.StandardWebSocketClient;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -16,6 +19,10 @@ import org.springframework.web.socket.messaging.WebSocketStompClient;
 public class WebsocketConfig implements  WebSocketMessageBrokerConfigurer {
     private static final int MAX_TEXT_MESSAGE_BUFFER_SIZE = 1024 * 1024;
 
+
+    @Autowired
+    private AccessManager accessManager;
+
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.enableSimpleBroker("/topic");
@@ -24,7 +31,13 @@ public class WebsocketConfig implements  WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/wsapps");
+        registry.addEndpoint("/wsapps").setAllowedOrigins("http://webprotege-local.edu");
+
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(new ProjectEventsInterceptor(accessManager));
     }
 
     @Bean
