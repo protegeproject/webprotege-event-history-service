f66e473ab20e6f3ca50cdb4b0863bfd9665807e0
soimugeo <soimugeo@gmail.com>
2024-11-12 12:26:23 +0200

    fixed an issue with the compareTo in ProjectChangeForEntity

--- src/main/java/edu/stanford/protege/webprotegeeventshistory/uiHistoryConcern/dto/ProjectChangeForEntity.java
+++ src/main/java/edu/stanford/protege/webprotegeeventshistory/uiHistoryConcern/dto/ProjectChangeForEntity.java
@@ -23,7 +23,10 @@ public record ProjectChangeForEntity(String whoficEntityIri,
 
     @Override
     public int compareTo(@Nonnull ProjectChangeForEntity other) {
-        return Long.compare(this.projectChange.getTimestamp(), other.projectChange.getTimestamp());
-
+        int timestampComparison = Long.compare(this.projectChange.getTimestamp(), other.projectChange.getTimestamp());
+        if (timestampComparison != 0) {
+            return timestampComparison;
+        }
+        return this.whoficEntityIri.compareTo(other.whoficEntityIri);
     }
 }
